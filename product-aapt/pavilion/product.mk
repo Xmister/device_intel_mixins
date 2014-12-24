924adcfedf027d14627ff8496daf85f93d3491d5
Xmister <xmisterhu@gmail.com>
2014-12-24 18:50:16 +0100

    Update Denisty
    Signed-off-by: Xmister <xmisterhu@gmail.com>

---
+++ product-aapt/pavilion/product.mk
@@ -0,0 +1,6 @@
+# This device is xhdpi.  However the platform doesn't
+# currently contain all of the bitmaps at xhdpi density so
+# we do this little trick to fall back to the hdpi version
+# if the xhdpi doesn't exist.
+PRODUCT_AAPT_CONFIG := normal hdpi xhdpi
+PRODUCT_AAPT_PREF_CONFIG := xhdpi
